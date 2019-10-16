59d8f28ebeb9dd38be33d0a2165a6785c7ced3d4
zhouxun <806275025@qq.com>
2019-10-16 17:01:52 +0800

    add random method

---
+++ xrandom/xrandom.go
@@ -0,0 +1,55 @@
+package xrandom
+
+import (
+	"bytes"
+	"math/rand"
+	"time"
+)
+
+const (
+	KC_RAND_KIND_NUM   = 0 // 纯数字
+	KC_RAND_KIND_LOWER = 1 // 小写字母
+	KC_RAND_KIND_UPPER = 2 // 大写字母
+	KC_RAND_KIND_ALL   = 3 // 数字、大小写字母
+)
+
+func StringJoint(strs ...string) string {
+	b := bytes.Buffer{}
+	for _, str := range strs {
+		b.WriteString(str)
+	}
+	return b.String()
+}
+
+func Krand(size int, kind int) string {
+	if size < 1 {
+		return ""
+	}
+	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
+	is_all := kind > 2 || kind < 0
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		if is_all { // random ikind
+			ikind = rand.Intn(3)
+		}
+		scope, base := kinds[ikind][0], kinds[ikind][1]
+		result[i] = uint8(base + rand.Intn(scope))
+	}
+	return string(result)
+}
+
+func RandomString(size int) string {
+	return Krand(size, KC_RAND_KIND_ALL)
+}
+
+func RandomUpper(size int) string {
+	return Krand(size, KC_RAND_KIND_UPPER)
+}
+
+func RandomLower(size int) string {
+	return Krand(size, KC_RAND_KIND_LOWER)
+}
+
+func RandomNumber(size int) string {
+	return Krand(size, KC_RAND_KIND_NUM)
+}
